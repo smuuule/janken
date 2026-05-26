2173a065a30dfcba56a9a884816d725fc1fdaa07
Samuel Runmark Thunell <samuel.runmark@gmail.com>
2026-05-26 09:44:56 +0200

    feat: initial project, maven/spring-boot setup

---
+++ src/test/java/dev/smule/janken/JankenApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.smule.janken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankenApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
